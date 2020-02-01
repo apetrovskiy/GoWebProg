35119946e630eaf1db087a52f4689652da92326f
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-02-01 03:40:41 +0300

    Feature/experimental generated unit tests (#4)
    * Develop (#2)
    
    * the initial commit (#1)
    
    * Update go.yml
    
    * Update go.yml
    
    * experimental unit tests

---
+++ first_webapp/server_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+func Test_handler(t *testing.T) {
+	/*
+	type args struct {
+		writer  http.ResponseWriter
+		request *http.Request
+	}
+	*/
+	tests := []struct {
+		name string
+		args *args
+	}{
+		// TODO: Add test cases.
+	//	{ "test01", NewArgs(http.ResponseWriter, new(http.Request))},
+	//	{ "test02", new(args)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			handler(tt.args.writer, tt.args.request)
+		})
+	}
+}
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
+}
+
+type args struct {
+	writer  http.ResponseWriter
+	request *http.Request
+}
+
+func NewArgs(writer  http.ResponseWriter, request *http.Request) *args {
+	m := new(args)
+	m.writer = writer
+	m.request = request
+	return m
+}
